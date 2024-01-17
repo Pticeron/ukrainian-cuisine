1ae052d6705f9f2d81617c2d732fd2a9d29891da
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2024-01-17 19:33:36 +0200

    add firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCSZ83LwxEdx13IsGcmk6M9D17Vm8KqrAk",
+//   authDomain: "ukrainian-cuisine.firebaseapp.com",
+//   projectId: "ukrainian-cuisine",
+//   storageBucket: "ukrainian-cuisine.appspot.com",
+//   messagingSenderId: "1069600562342",
+//   appId: "1:1069600562342:web:0062ad30c6c0c44a186867",
+//   measurementId: "G-ZFBW8FYT9W"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
